d7a30b5c8538d50ea113b1ac22bc6ca317a9882c
Aleksey1123 <aleksey.rytov@gmail.com>
2024-07-13 18:49:26 +0300

    adding spring aop + logging

--- homeworks/hw4/src/main/java/org/example/config/AppConfig.java
+++ homeworks/hw4/src/main/java/org/example/config/AppConfig.java
@@ -2,12 +2,15 @@ package org.example.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.example.annotation.LoggableAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = "org.example")
+@EnableAspectJAutoProxy
 public class AppConfig {
 
     @Bean
@@ -15,5 +18,10 @@ public class AppConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
+    }
+
+    @Bean
+    public LoggableAspect loggableAspect() {
+        return new LoggableAspect();
     }
 }
